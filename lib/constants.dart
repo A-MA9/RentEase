bd61b01bb7cc852092284d43bf0e469b76b8c9d7
AMA-9 <bhartiaman929@gmail.com>
2025-04-11 12:38:41 +0530

    Name

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'dart:io' show Platform;
 
 // Simplified to a constant since we're using the same URL for all platforms
-const String baseUrl = "https://b82a-49-249-159-114.ngrok-free.app";
+const String baseUrl = "https://rentease-moz4.onrender.com";
 
 // OTP URL is the same as base URL
-const String otpUrl = "https://b82a-49-249-159-114.ngrok-free.app";
+const String otpUrl = "https://rentease-moz4.onrender.com";
